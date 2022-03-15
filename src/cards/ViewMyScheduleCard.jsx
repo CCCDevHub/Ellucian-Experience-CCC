f2394457734412c1cb6bea11feea91434659236a
Huey Phan <hphan10@pasadena.edu>
2022-03-15 11:22:59 -0700

    Got the schedule and data show up in calendar

--- src/cards/ViewMyScheduleCard.jsx
+++ src/cards/ViewMyScheduleCard.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect, Fragment, Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@ellucian/react-design-system/core/styles';
 import { spacing10, spacing40 } from '@ellucian/react-design-system/core/styles/tokens';
-import { Typography, Button, List, ListItem, Calendar, CalendarLegend, CalendarLegendItem, views } from '@ellucian/react-design-system/core';
+import { Typography, Button, TextLink, List, ListItem, Calendar, CalendarLegend, CalendarLegendItem, views, momentLocalizer } from '@ellucian/react-design-system/core';
 import moment from 'moment';
-
+import { saffron600 } from '@ellucian/react-design-system/core/styles/tokens';
 
 const styles = () => ({
     card: {
@@ -31,13 +31,23 @@ const ViewMySchedule = (props) => {
 
     const textMessage = configuration.message;
     const [schedule, setSchedule] = useState();
+    const [event, setEvent] = useState({});
+
+    const days = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
+    const sectionsEvents = [];
+    const dayList = [];
+
+    const onSelectEvent = (e) => {
+        setEvent(e);
+    };
 
     useEffect(() => {
         (async () => {
             setLoadingStatus(true)
             try {
                 // Get Etho Query from index.js
-                const result = await getEthosQuery({ queryId: 'list-schedule' });
+                const result = await getEthosQuery({ queryId: 'schedule-list' });
+
                 // Destruct to Edges
                 const scheduleEdges = (result?.data?.instructionalEvents?.edges || []);
 
@@ -61,37 +71,75 @@ const ViewMySchedule = (props) => {
         }
         )();
     }, []);
-    const sections = [];
+
+
+    for (const item in schedule) {
+        if (item) {
+            const { recurrence: { repeatRule: { type: repeatType, daysOfWeek }, timePeriod: { startOn, endOn } }, section: { titles: classTitle }, locations } = schedule[item];
+            const { room: { number: roomNum, building }, site } = locations[0].location
+
+            for (const day in daysOfWeek) {
+                if (day) {
+                    dayList.push(daysOfWeek[day].toUpperCase())
+                }
+            }
+
+            const startDate = new Date('2022, 01, 01');
+            const endDate = new Date('2022, 06, 01');
+
+            let loop = new Date(startDate);
+
+            while (loop <= endDate) {
+                if (dayList.includes(days[loop.getDay()]) && repeatType === 'weekly') {
+                    const classEvent = {
+                        title: classTitle[0].value,
+                        start: new Date(loop),
+                        end: new Date(loop),
+                        room: building.title + ' ' + roomNum
+                    }
+                    sectionsEvents.push(classEvent);
+                }
+                const newDate = loop.setDate(loop.getDate() + 1);
+                loop = new Date(newDate);
+            }
+
+        }
+    }
+
+    const testEvent =
+    {
+        title: 'MIT200',
+        allDay: true,
+        start: new Date('2022, 03, 16'),
+        end: new Date('2022, 03, 16')
+    };
+
+    const TestCalendar = (schedule) => {
+        moment.locale('en')
+        return (
+            <div>
+                <Calendar
+                    defaultView={views.DAY}
+                    views={[views.DAY, views.WORK_WEEK]}
+                    timeslots={1}
+                    step={60}
+                    events={sectionsEvents}
+                    style={{ height: 900 }}
+                    min={moment('7:00am', 'h:mma').toDate()}
+                    max={moment('9:00pm', 'h:mma').toDate()}
+                    onSelectEvent={onSelectEvent}
+                    onKeyPressEvent={onSelectEvent}
+                    popperContent={
+                        <h1>TEST</h1>
+                    }
+                />
+            </div >
+        )
+    }
+
     return (
         <Fragment>
-            {schedule && (
-                <div className={classes.cards}>
-
-                    {schedule.map((schedule) => {
-                        const { recurrence: { repeatRule: { type, daysOfWeek }, timePeriod: { startOn, endOn } }, section: { titles: classTitle }, locations } = schedule;
-                        const { room: { number: roomNum, building }, site } = locations[0].location
-
-                        const listDay = daysOfWeek.map((day) =>
-                            <ListItem key={day}>{day}</ListItem>
-                        )
-                        console.log(schedule.section)
-                        return (
-                            <Typography key={schedule.id}>
-                                <List>
-                                    <ListItem><b>Class Title:</b> {classTitle[0].value}</ListItem>
-                                    <ListItem><b>Day of Week:</b> {listDay} </ListItem>
-                                    <ListItem><b>Start:</b> {startOn} </ListItem>
-                                    <ListItem><b>End:</b> {endOn} </ListItem>
-                                    <ListItem><b>Repeat Type:</b> {type} </ListItem>
-                                    <ListItem><b>Building:</b> {building.title} </ListItem>
-                                    <ListItem><b>Room:</b> {roomNum} </ListItem>
-                                    <ListItem><b>Campus:</b> {site} </ListItem>
-                                </List>
-                            </Typography>
-                        )
-                    })}
-                </div>
-            )}
+            <TestCalendar />
         </Fragment >
     );
 };
