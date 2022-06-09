8809308305dfe268e1d09b0cb8431be0fa095c20
Huey Phan <hphan10@pasadena.edu>
2022-06-09 09:59:20 -0700

    Added working extension for Grade Assignment

--- extension.js
+++ extension.js
@@ -1,82 +1,55 @@
 module.exports = {
-    "name": "Grades Assignment",
+    "name": "Grade Assignment",
     "publisher": "Huey Phan",
     "version": "1.0.0",
     "cards": [{
         "type": "GraphQLQueryCard",
         "source": "./src/cards/GradeAssignmentCard",
-        "title": "Grades Assignment",
+        "title": "Grade Assignment",
         "displayCardType": "GraphQL Grade Assignment",
-        "description": "Grades Assignment",
-        "pageRoute" : {
-            "route": "/",
-            "excludeClickSelectors": ['#link']
-        },
+        "description": "Grade Assignment",
         "queries": {
             "section-list": [
                 {
                     "resourceVersions": {
                         "sections": {min: 16},
+                        "reportingAcademicPeriods" : {min: 16},
                         "courses": {min: 16},
                         "subjects": {min: 6},
-                        "sectionStatuses" : {min: 11},
-                        "academicPeriods": { min: 16},
-                        "sectionInstructors": {min: 10},
-                        "persons": {min: 12}
+                        "SectionStatuses" : {min: 11}
                     },
                     "query":
-                        `query sectionList($personId: ID, $todayDate: Date){
-                            	sectionInstructors: {sectionInstructors} (
+                        `query sectionList($sectionId: ID){
+                            sections: {sections} (
                                     filter: {
-                                        {instructor@persons} : { id: { EQ: $personId } }
-                                        {section@sections}: { 
-                                                            startOn: {  LAST_DAYS: 90 }
-                                                            }
+                                        id: {EQ: $sectionId}
                                     }
-                                    sort: {{section@sections}:{{reportingAcademicPeriod@academicPeriods}:{code:DESC}}}
-                                ) {
+                                )
+                                {
                                     edges {
                                         node {
                                             id
-                                            section16 {
-                                                id
-                                                startOn
-                                                endOn
-                                                titles {
-                                                    value
-                                                }
-                                                code
-                                                reportingAcademicPeriod16 {
-                                                    code
-                                                    title
-                                                    registration
-                                                }
-                                                status {
-                                                    category
-                                                    detail11 {
-                                                        title
-                                                        category
-                                                        description
-                                                    }
-                                                }
-                                                maxEnrollment
-                                                crossListed
-                                                alternateIds {
-                                                    title
-                                                    value
-                                                }
-                                                course16 {
-                                                    subject: {subject} {
-                                                        abbreviation
-                                                    }
-                                                    number
-                                                }
+                                            titles {
+                                                value
                                             }
-                                            instructor12 {
-                                                id
-                                                names {
-                                                    fullName
+                                            code
+                                            reportingAcademicPeriod: {reportingAcademicPeriods} {
+                                                code
+                                                title
+                                                registration
+                                            }
+         
+                                            maxEnrollment
+                                            crossListed
+                                            alternateIds {
+                                                title
+                                                value
+                                            }
+                                            course: {courses} {
+                                                subject: {subjects} {
+                                                    abbreviation
                                                 }
+                                                number
                                             }
                                         }
                                     }
@@ -87,6 +60,6 @@ module.exports = {
         }
     }],
     "page": {
-        "source": "./src/page/gradeAssignmentPage.jsx"
+        "source": "./src/page/index.jsx"
     }
 }
--- src/cards/GradeAssignmentCard.jsx
+++ src/cards/GradeAssignmentCard.jsx
@@ -1,19 +1,8 @@
 import React, {useState, useEffect, Fragment} from "react";
 import PropTypes from "prop-types";
-import {spacing10, spacing40, widthFluid} from '@ellucian/react-design-system/core/styles/tokens';
+import {spacing10, spacing40} from '@ellucian/react-design-system/core/styles/tokens';
 import {withStyles} from '@ellucian/react-design-system/core/styles';
-import {
-    Table,
-    TableHead,
-    TableRow,
-    TableCell,
-    TableBody,
-    Typography,
-    TextLink
-} from '@ellucian/react-design-system/core';
-import {useCardInfo} from "@ellucian/experience-extension/extension-utilities";
-
-const cacheKey = 'section-table-data';
+import {Typography} from '@ellucian/react-design-system/core';
 
 const styles = () => ({
     card: {
@@ -24,141 +13,69 @@ const styles = () => ({
     text: {
         marginRight: spacing40,
         marginLeft: spacing40
-    },
-    root: {
-        width: widthFluid,
-        paddingTop: spacing10,
-        overflowX: 'auto'
     }
 });
 
-const GradeAssignmentCard = (props) => {
+const CardName = (props) => {
     const {
         classes,
         data: {getEthosQuery},
         cardControl: {
             setLoadingStatus,
-            setErrorMessage,
-            setPreventRemove
-        },
-        cache: {
-            storeItem
-             }
-    } = props;
-    const { cardId } = useCardInfo();
-
-    let id = 0;
-    const [sectionData, setSectionData] = useState();
-    const tableData = [];
-
-    const todayDate = new Date().toJSON().slice(0, 10);
-    setPreventRemove(true);
-
-    useEffect(() => {
-        (async () => {
-                setLoadingStatus(true);
-                try {
-                    const sectionResult = await getEthosQuery({ queryId: 'section-list', properties: { todayDate: todayDate } });
-                    const sections = sectionResult?.data?.sections?.edges.map(edge => edge.node);
-
-                    setSectionData(() => sections);
-
-                    setLoadingStatus(false);
-                } catch (error) {
-                    console.log('ethosQuery failed', error);
-                    setErrorMessage({
-                        headerMessage: ({id: 'GraphQLQueryCard-fetchFailed'}),
-                        textMessage: ({id: 'GraphQLQueryCard-classesFetchFailed'}),
-                        iconName: 'error',
-                        iconColor: '#D42828'
-                    });
-                }
-            }
-        )();
-    }, []);
-
-    for (const i in sectionData) {
-        if (i != null) {
-            const {alternateIds, code: crn, course:{number:csn, subject:{abbreviation:dept}}, maxEnrollment, reportingAcademicPeriod16: {code: termCode, registration, title: termName}, status: {detail11:{category, title:statusTitle}}, titles} = sectionData[i];
-            tableData.push(createData(statusTitle, titles[1].value, dept, csn, termName, crn, maxEnrollment, termCode));
+            setErrorMessage
         }
-    }
-    if (tableData !== undefined || tableData.length !== 0){
-        storeItem({ key: cacheKey, data: tableData, scope: cardId });
-    }
+    } = props;
 
-    function createData(status, title, dept, csn, term, crn, enrolled, termCode) {
-        id += 1;
-        return {id, status, title, dept, csn, term, crn, enrolled, termCode};
-    }
-
-    if (tableData.length !== 0) {
-        return (
-                <div className={classes.root}>
-                    <Typography>
-                    <Table layout={{ variant: 'card', breakpoint: 'sm'}}>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Status</TableCell>
-                            <TableCell>Course Title</TableCell>
-                            <TableCell>Dept</TableCell>
-                            <TableCell>CSN</TableCell>
-                            <TableCell>Term</TableCell>
-                            <TableCell>CRN</TableCell>
-                            <TableCell>Session</TableCell>
-                            <TableCell>Enrolled</TableCell>
-                        </TableRow>
-                    </TableHead>
-                        <TableBody>
-                            {tableData.map(n => {
-                                return (
-                                    <TableRow key={n.id}>
-                                        <TableCell columnName="Status">
-                                            {n.status}
-                                        </TableCell>
-                                        <TableCell columnName="Course Title">
-                                            <TextLink id="link"
-                                                      href={`https://ssb-prod.ec.pasadena.edu/PROD/bwlkffgd.P_FacFinGrd?TERM=${n.termCode}&CRN=${n.crn}`}>
-                                                {n.title}
-                                            </TextLink>
-                                        </TableCell>
-                                        <TableCell columnName="Dept">
-                                            {n.dept}
-                                        </TableCell>
-                                        <TableCell columnName="CSN">
-                                            {n.csn}
-                                        </TableCell>
-                                        <TableCell columnName="Term">
-                                            {n.term}
-                                        </TableCell>
-                                        <TableCell columnName="CRN">
-                                            {n.crn}
-                                        </TableCell>
-                                        <TableCell columnName="Session">
-                                            -
-                                        </TableCell>
-                                        <TableCell columnName="Enrolled">
-                                            {n.enrolled}
-                                        </TableCell>
-                                    </TableRow>
-                                )
-                            })}
-                        </TableBody>
-                    </Table>
-                    </Typography>
-                </div>
-        );
-    }
-    else {
-        return(null);
-    }
+    const [ethoDatas, setEthoDatas] = useState();
+    const sectionIdList = ["cb9475e2-69ee-437e-88ff-6e190f30d282",
+        "581fee9e-5e7d-41c8-8205-3929a47f411e",
+        "00531855-6397-441f-b876-8efd87ec8c65",
+        "ef1b341b-6776-4834-8447-462eac7fd5ad",
+        "0648f603-7196-48bb-9c45-c2f3863202a5",
+        "e1b808f7-2ec7-4b31-88c0-08c10ba0433b",
+        "3ec2dfc8-0147-418e-86b1-d4d1ad1aad3d",
+        "2367fc5c-75b9-4b4e-9c43-59c6f7bbcf1a",
+        "9df38eb1-846e-4d1b-95a3-996068c0f153"]
+    // useEffect(() => {
+    //     (async () => {
+    //             setLoadingStatus(true);
+    //             try {
+    //                 const sectionPromises = [];
+    //                 console.log('try to get data');
+    //                 const promise = getEthosQuery({queryId: 'section-list', properties: {sectionId: "cb9475e2-69ee-437e-88ff-6e190f30d282"}});
+    //                 console.log('you shall not pass');
+    //                 sectionPromises.push(promise);
+    //
+    //                 const sectionResult = await Promise.all(sectionPromises);
+    //                 console.log(sectionResult);
+    //                 setLoadingStatus(false);
+    //             } catch (error) {
+    //                 console.log('ethosQuery failed', error);
+    //                 setErrorMessage({
+    //                     headerMessage: ({id: 'GraphQLQueryCard-fetchFailed'}),
+    //                     textMessage: ({id: 'GraphQLQueryCard-classesFetchFailed'}),
+    //                     iconName: 'error',
+    //                     iconColor: '#D42828'
+    //                 });
+    //             }
+    //         }
+    //     )();
+    // }, []);
+    return (
+        <Fragment>
+            <div className={classes.card}>
+                <Typography>
+                    <h1>Test</h1>
+                </Typography>
+            </div>
+        </Fragment>
+    )
 };
 
-GradeAssignmentCard.propTypes = {
+CardName.propTypes = {
     classes: PropTypes.object.isRequired,
     cardControl: PropTypes.object.isRequired,
-    data: PropTypes.object.isRequired,
-    cache: PropTypes.object.isRequired
+    data: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(GradeAssignmentCard);
+export default withStyles(styles)(CardName);
