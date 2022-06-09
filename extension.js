cfbc3854a17811f4fe7a376107ff6e20b4b8e188
Huey Phan <hphan10@pasadena.edu>
2022-06-09 09:59:20 -0700

    Added working extension for Grade Assignment

--- extension.js
+++ extension.js
@@ -1,18 +1,65 @@
 module.exports = {
-    "name": "Extension Name",
-    "publisher": "Your Name",
+    "name": "Grade Assignment",
+    "publisher": "Huey Phan",
+    "version": "1.0.0",
     "cards": [{
-        "type": "TestExtCard",
-        "source": "./src/cards/CardName",
-        "title": "Card Title",
-        "displayCardType": "Card Type",
-        "description": "This is an introductory card to the Ellucian Experience SDK",
-        "pageRoute": {
-            "route": "/",
-            "excludeClickSelectors": ['a']
+        "type": "GraphQLQueryCard",
+        "source": "./src/cards/GradeAssignmentCard",
+        "title": "Grade Assignment",
+        "displayCardType": "GraphQL Grade Assignment",
+        "description": "Grade Assignment",
+        "queries": {
+            "section-list": [
+                {
+                    "resourceVersions": {
+                        "sections": {min: 16},
+                        "reportingAcademicPeriods" : {min: 16},
+                        "courses": {min: 16},
+                        "subjects": {min: 6},
+                        "SectionStatuses" : {min: 11}
+                    },
+                    "query":
+                        `query sectionList($sectionId: ID){
+                            sections: {sections} (
+                                    filter: {
+                                        id: {EQ: $sectionId}
+                                    }
+                                )
+                                {
+                                    edges {
+                                        node {
+                                            id
+                                            titles {
+                                                value
+                                            }
+                                            code
+                                            reportingAcademicPeriod: {reportingAcademicPeriods} {
+                                                code
+                                                title
+                                                registration
+                                            }
+         
+                                            maxEnrollment
+                                            crossListed
+                                            alternateIds {
+                                                title
+                                                value
+                                            }
+                                            course: {courses} {
+                                                subject: {subjects} {
+                                                    abbreviation
+                                                }
+                                                number
+                                            }
+                                        }
+                                    }
+                                }
+                        }`
+                }
+            ]
         }
     }],
     "page": {
-        "source": "./src/page/router.jsx"
+        "source": "./src/page/index.jsx"
     }
 }
