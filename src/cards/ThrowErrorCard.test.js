a1283e405bda3f9845d98d29b6d569ab6b80fd85
Huey Phan <90303106+hphan10@users.noreply.github.com>
2022-09-02 10:55:54 -0700

    Delete src/cards directory

--- src/cards/ThrowErrorCard.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ThrowErrorCard from './ThrowErrorCard';
-import {shallow} from 'enzyme';
-
-describe('<ThrowErrorCard>', () => {
-    it('Throws an error', () => {
-        expect(() => shallow(<ThrowErrorCard />)).toThrowError();
-    });
-});
