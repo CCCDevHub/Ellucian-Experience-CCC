2d42dabec20cd3b07c12114d19d0c716b3b9c57d
hphan10 <hphan10@pasadena.edu>
2024-07-03 09:56:05 -0700

    - Update code to use custom api and data connect instead of extensions

--- extension.js
+++ extension.js
@@ -1,139 +1,24 @@
 module.exports = {
-    "name": "experienceWorkshopPaulaBaltazar",
-    "publisher": "PaulaBaltazar",
-    "cards": [{
-        "type": "Milestones",
-        "source": "./src/cards/Milestones",
-        "title": "Getting Started at PCC",
-        "displayCardType": "GraphQL Query",
-        "description": "Milestones",
-        "queries": {
-            "person-ext": [
-                {
-                    "resourceVersions": {
-                        "persons": { min: 12 }
-                    },
-                    "query":
-                        `query personInfo($personId: ID){
-                            persons: {persons} (
-                                    filter: {
-                                        id: {EQ: $personId}
-                                    }
-                                )
-                                {
-                                    edges {
-                                        node {
-                                            id
-                                            gender
-                                            names {
-                                                fullName
-                                            }
-                                            dateOfBirth
-                                            veteranStatus {
-                                                category
-                                            }
-                                            citizenshipStatus {
-                                                category
-                                            }
-                                            roles {
-                                                role
-                                            }
-                            
-                                            credentials {
-                                                value
-                                                type
-                                            }
-                            
-                                            emails {
-                                                type {
-                                                    emailType
-                                                }
-                                                address
-                                            }
-                                            extensions{
-                                                milestonesRegpri
-                                                milestonesEdplans
-                                                milestonesAssessments
-                                                milestonesOrientation
-                                            }
-                                        }
-                                    }
-                                }
-                        }`
-                }
-            ]
-            // "person-hold": [
-            //     {
-            //         "resourceVersions": {
-            //             "persons": { min: 12 },
-            //             "personHolds": { min: 6 }
-            //         },
-            //         "query":
-            //             `query personHoldInfo($personId : ID, $today: Date) {
-            //                 personHolds: {personHolds} (
-            //                     filter:{
-            //                         {person@persons}: { id: { EQ: $personId } }
-            //                         endOn: {AFTER: $today} 
-            //                         startOn: {BEFORE: $today}
-            //                     }
-            //                 ) {
-            //                     edges {
-            //                         node {
-            //                             id
-            //                             type {
-            //                                 detail6 {
-            //                                     title
-            //                                     category
-            //                                     description
-            //                                 }
-            //                             }
-            //                             startOn
-            //                             endOn
-            //                         }
-            //                     }
-            //                 }
-            //             }
-            //             `
-            //     }
-            // ],
-            // "student-tags": [
-            //     {
-            //         "resourceVersions": {
-            //             "persons": { min: 12 },
-            //             "studentTags": { min: 7 },
-            //             "studentTagAssignments": { min: 1 }
-            //         },
-            //         "query":
-            //             `query studentTagInfo($personId : ID, $today: Date) {
-            //                 studentTagAssignments: {studentTagAssignments} (
-            //                     filter:{
-            //                         {person@persons}: {
-            //                             id: {EQ: $personId}
-            //                         }
-            //                         {tag@studentTags}: { code: { IN: ["OUT", "NSV"] } }
-            //                         startOn:{ AFTER: $today} endOn:{BEFORE: $today}
-            //                     }
-            //                 ) {
-            //                    edges {
-            //                         node {
-            //                             id
-            //                             tag7 {
-            //                                 title
-            //                                 description
-            //                                 code
-            //                             }
-            //                             startOn
-            //                             endOn
-            //                             person12 {
-            //                                 id
-            //                             }
-            //                         }
-            //                     }
-            //                 }
-            //             }
-            //             `
-            //     }
-            // ]
+    name: 'experienceWorkshopPaulaBaltazar',
+    publisher: 'PaulaBaltazar',
+    cards: [{
+        type: 'Milestones',
+        source: './src/cards/GetStartedWithPCC',
+        title: 'Getting Started at PCC',
+        displayCardType: 'GraphQL Query',
+        description: 'Milestones',
+        configuration: {
+            client: [{
+                key: 'pipelineAPI',
+                label: 'Pipeline API',
+                type: 'text'
+            }],
+            server: [{
+                key: 'ethosApiKey',
+                label: 'Ethos API Key',
+                type: 'password',
+                required: true
+            }]
         }
     }]
 };
--- src/cards/GetStartedWithPCC.jsx
+++ src/cards/GetStartedWithPCC.jsx
@@ -3,8 +3,7 @@ import PropTypes from "prop-types";
 import { spacing40 } from '@ellucian/react-design-system/core/styles/tokens';
 import { withStyles } from '@ellucian/react-design-system/core/styles';
 import { Icon } from '@ellucian/ds-icons/lib';
-import { Button, IconButton } from "@ellucian/react-design-system/core";
-import { Popover } from '@material-ui/core';
+import { Button, IconButton, Popover } from "@ellucian/react-design-system/core";
 
 const styles = () => ({
     card: {
@@ -44,25 +43,28 @@ const customId = 'SimpleButtons';
 const MyInfo = (props) => {
     const {
         classes,
-        cardInfo: { configuration },
-        data: { getEthosQuery },
+        cardInfo: { configuration: { pipelineAPI }, cardId },
+        data: { getEthosQuery, authenticatedEthosFetch },
         cardControl: {
             setLoadingStatus,
             setErrorMessage,
             setPreventRemove
-        }
+        },
+        userInfo: { roles }
     } = props;
 
+    const personId = roles.pop();
     const [persons, setPersons] = useState();
+    const [checkLists, setCheckLists] = useState();
     const [anchorEl, setAnchorEl] = useState(null);
     const [popoverid, setPopoverid] = useState();
 
-     const handlePopoverOpen = (event, popid) => {
-         setAnchorEl(event.currentTarget);
-         setPopoverid(popid);
-     };
+    const handlePopoverOpen = (event, popid) => {
+        setAnchorEl(event.currentTarget);
+        setPopoverid(popid);
+    };
 
-     const handlePopoverClose = () => {
+    const handlePopoverClose = () => {
         setAnchorEl(null);
         setPopoverid(null);
     };
@@ -73,14 +75,17 @@ const MyInfo = (props) => {
         (async () => {
             setLoadingStatus(true);
             try {
-                // Get person info
-                const personResult = await getEthosQuery({
-                    queryId: 'person-ext'
-                });
-                const personData = (personResult?.data?.persons?.edges.map(edge => edge.node));
-                console.log('persondata', personData);
+                const gettingStarted = await authenticatedEthosFetch(`${pipelineAPI}?cardId=${cardId}&testPersonId=${personId}`);
+                const gettingStartedResult = await gettingStarted.json()
+                setCheckLists(() => gettingStartedResult);
+                // // Get person info
+                // const personResult = await getEthosQuery({
+                //     queryId: 'person-ext'
+                // });
+                // const personData = (personResult?.data?.persons?.edges.map(edge => edge.node));
+                // console.log('persondata', personData);
 
-                setPersons(() => personData);
+                // setPersons(() => personData);
 
                 setLoadingStatus(false);
             } catch (error) {
@@ -97,79 +102,79 @@ const MyInfo = (props) => {
     }, []);
 
     // Check if all the data is loaded
-    if (persons) {
-        const person = destructPersonData(persons);
+    if (checkLists) {
+        // const person = destructPersonData(persons);
 
         // check if all milestones are complete
-        const c = person[0];
-
+        // const c = person[0];
+        const c = destructData(checkLists[0]);
         return (
-        <div className={classes.card}>
+            <div className={classes.card}>
 
-            {/* Comment: Display "Early Registration" badge if all milestones are completed */}
-            <div className={classes.completeBadge}>
-                <img src={c.complete ? "https://admin.pasadena.edu/dev/badges/images/reg_pri.png" : ""} alt=""></img>
+                {/* Comment: Display "Early Registration" badge if all milestones are completed */}
+                <div className={classes.completeBadge}>
+                    <img src={c.complete ? "https://admin.pasadena.edu/dev/badges/images/reg_pri.png" : ""} alt=""></img>
+                </div>
+
+                <ul>
+                    {c.map(m => (
+                        <li key={m.id}
+                            style={{
+                                listStyleType: "none",
+                                display: "flex",
+                                alignItems: "start"
+
+                            }}
+                        >
+                            <Button
+                                id={`${customId}_PrimaryButton`}
+                                className={m.complete ? classes.completeButton : classes.button}
+                                href={m.linkURL}
+                                color={m.buttoncolor}
+                                fullWidth
+                            >
+                                <div>
+                                    <Icon name={m.iconname} className={classes.iconRoot} color={m.iconcolor} />
+                                    {m.name}
+                                </div>
+                            </Button>
+
+                            <IconButton
+                                color="black"
+                                id={`${customId}_GrayButtonBlueBackground`}
+                                aria-label="help"
+                                className={classes.buttonSpacing}
+                                onClick={(e) => handlePopoverOpen(e, m.id)}
+                            >
+                                <Icon name="help" />
+                            </IconButton>
+
+                            <Popover
+                                className={classes.popover}
+                                classes={{
+                                    paper: classes.paper
+                                }}
+                                open={popoverid === m.id}
+                                onClose={handlePopoverClose}
+                                anchorEl={anchorEl}
+                                transformOrigin={{
+                                    horizontal: "left",
+                                    vertical: "center"
+                                }}
+                                anchorOrigin={{
+                                    horizontal: "right",
+                                    vertical: "center"
+                                }}
+                            >
+                                {m.hovertext}
+                            </Popover>
+                        </li>
+                    ))}
+                </ul>
+                <div className={classes.iconRoot}>
+                    <p><b>Note:</b> High School Dual or Concurrent Enrollment students do not need to complete these badges to register.</p>
+                </div>
             </div>
-
-            <ul>
-                {person.map(m => (
-                    <li key={m.id}
-                        style={{
-                            listStyleType: "none",
-                            display: "flex",
-                            alignItems: "start"
-
-                        }}
-                    >
-                        <Button
-                            id={`${customId}_PrimaryButton`}
-                            className={m.complete ? classes.completeButton : classes.button}
-                            href={m.linkURL}
-                            color={m.buttoncolor}
-                            fullWidth
-                        >
-                            <div>
-                                <Icon name={m.iconname} className={classes.iconRoot} color={m.iconcolor}/>
-                                {m.name}
-                            </div>
-                        </Button>
-
-                        <IconButton
-                            color="black"
-                            id={`${customId}_GrayButtonBlueBackground`}
-                            aria-label="help"
-                            className={classes.buttonSpacing}
-                            onClick={(e) => handlePopoverOpen(e, m.id)}
-                        >
-                            <Icon name="help" />
-                        </IconButton>
-
-                        <Popover
-                            className={classes.popover}
-                            classes={{
-                                paper: classes.paper
-                            }}
-                            open={popoverid === m.id}
-                            onClose={handlePopoverClose}
-                            anchorEl={anchorEl}
-                            transformOrigin={{
-                                horizontal: "left",
-                                vertical: "center"
-                            }}
-                            anchorOrigin={{
-                                horizontal: "right",
-                                vertical: "center"
-                            }}
-                        >
-                            {m.hovertext}
-                        </Popover>
-                    </li>
-                ))}
-            </ul>
-            <div className={classes.iconRoot}>
-                <p><b>Note:</b> High School Dual or Concurrent Enrollment students do not need to complete these badges to register.</p>
-            </div>
-        </div>
         );
     }
     else {
@@ -177,71 +182,114 @@ const MyInfo = (props) => {
     }
 };
 
-function destructPersonData(persons) {
+function createData(id, name, value, hovertext, linkURL, buttoncolor, iconname, iconcolor, complete) {
+    return { id, name, value, hovertext, linkURL, buttoncolor, iconname, iconcolor, complete };
+}
+
+function createHoverText(isComplete, completedText, incompleteText, link) {
+    return isComplete ? completedText : (
+        <p>{incompleteText} <a rel="noreferrer" target="_blank" href={link}>{link}</a>.</p>
+    );
+}
+
+function getButtonColor(isComplete) {
+    return isComplete ? 'secondary' : 'primary';
+}
+
+function getIconProps(isComplete) {
+    return {
+        name: isComplete ? 'check-circle' : 'circle',
+        color: isComplete ? 'green' : 'white'
+    };
+}
+
+function checkAllMilestonesComplete(checklist) {
+    return checklist.orientation === "1" &&
+        checklist.assessment === "1" &&
+        checklist.edPlan === "1" &&
+        checklist.regPri === "1";
+}
+
+function destructData(checklist) {
     let id = 0;
-    function createData(name, value, hovertext, linkURL, buttoncolor, iconname, iconcolor, complete) {
-        id += 1;
-        return { id, name, value, hovertext, linkURL, buttoncolor, iconname, iconcolor, complete };
+
+    if (checkAllMilestonesComplete(checklist)) {
+        return [
+            createData(
+                ++id,
+                'Register',
+                'null',
+                'You have completed steps for early registration!',
+                'https://ssb-prod.ec.pasadena.edu/PROD/bwskflib.P_SelDefTerm',
+                'null',
+                'null',
+                'null',
+                1
+            )
+        ];
     }
 
-    if (persons) {
-        const {extensions} = persons[0];
-
-        // if all milestones are complete, show completed badge
-        if(extensions.milestonesOrientation === 1 &&
-            extensions.milestonesAssessments === 1  &&
-            extensions.milestonesEdplans === 1  &&
-            extensions.milestonesRegpri === 1 ) {
-            return [
-                createData('Register',
-                            'null',
-                            'You have completed steps for early registration!',
-                            'https://ssb-prod.ec.pasadena.edu/PROD/bwskflib.P_SelDefTerm',
-                            'null',
-                            'null',
-                            'null',
-                            1)
-            ]
-        }
-        else {
-            return [
-                createData('Orientation',
-                            extensions.milestonesOrientation,
-                            extensions.milestonesOrientation ? 'You have completed the online orientation.': <p>You have not completed your online orientation. Please visit <a rel="noreferrer" target="_blank" href="https://orientation.pasadena.edu"> https://orientation.pasadena.edu </a> to get started.</p>,
-                            'https://orientation.pasadena.edu',
-                            extensions.milestonesOrientation ? 'secondary':'primary',
-                            extensions.milestonesOrientation ? 'check-circle':'circle',
-                            extensions.milestonesOrientation ? "green":"white",
-                            0),
-                createData('Placement',
-                            extensions.milestonesAssessments,
-                            extensions.milestonesAssessments ? <p>You have completed your placement process. For help selecting appropriate classes visit: <a rel="noreferrer" target="_blank" href="https://pasadena.edu/academics/support/counseling/academic-planning/placement.php">https://pasadena.edu/academics/support/counseling/academic-planning/placement.php </a>.</p>
-                                        : <p>You have not completed the placement process. To know what English and math courses are right for you, get started by visiting <a rel="noreferrer" target="_blank" href="https://pasadena.edu/academics/support/counseling/academic-planning/placement.php">https://pasadena.edu/academics/support/counseling/academic-planning/placement.php </a>.</p>,
-                            'https://pasadena.edu/academics/support/counseling/academic-planning/placement.php',
-                            extensions.milestonesAssessments ? 'secondary':'primary',
-                            extensions.milestonesAssessments ? 'check-circle':'circle',
-                            extensions.milestonesAssessments ? "green":"white",
-                            0),
-                createData('Ed Plan',
-                            extensions.milestonesEdplans,
-                            extensions.milestonesEdplans ? <p>You have completed an educational plan. To view, please visit <a rel="noreferrer" target="_blank" href="https://pasadena.edu/academics/support/counseling/academic-planning/">https://pasadena.edu/academics/support/counseling/academic-planning/ </a>.</p>
-                                        : <p>Completing an educational plan will make sure you’re taking the correct classes. (You can register without a plan) To request a plan use this link: <a rel="noreferrer" target="_blank" href="https://pasadena.edu/academics/support/counseling/ask-a-counselor/new-students.php">https://pasadena.edu/academics/support/counseling/ask-a-counselor/new-students.php </a>.</p>,
-                            'https://pasadena.edu/academics/support/counseling/academic-planning/',
-                            extensions.milestonesEdplans ? 'secondary':'primary',
-                            extensions.milestonesEdplans ? 'check-circle':'circle',
-                            extensions.milestonesEdplans ? "green":"white",
-                            0),
-                createData('Early Registration',
-                            extensions.milestonesRegpri,
-                            extensions.milestonesRegpri ? 'You have completed steps for early registration!':'To get registration priority, you must first complete the online orientation, assessment, and Ed Plan.',
-                            'https://pasadena.edu/academics/support/counseling/academic-planning/placement.php',
-                            extensions.milestonesRegpri ? 'secondary':'primary',
-                            extensions.milestonesRegpri ? 'check-circle':'circle',
-                            extensions.milestonesRegpri ? "green":"white",
-                            0)
-            ];
-        }
-    }
+    return [
+        createData(
+            ++id,
+            'Orientation',
+            checklist.orientation,
+            createHoverText(
+                checklist.orientation === "1",
+                'You have completed the online orientation.',
+                'You have not completed your online orientation. Please visit ',
+                'https://orientation.pasadena.edu'
+            ),
+            'https://orientation.pasadena.edu',
+            getButtonColor(checklist.orientation === "1"),
+            getIconProps(checklist.orientation === "1").name,
+            getIconProps(checklist.orientation === "1").color,
+            0
+        ),
+        createData(
+            ++id,
+            'Placement',
+            checklist.assessment,
+            createHoverText(
+                checklist.assessment === "1",
+                'You have completed your placement process. For help selecting appropriate classes visit: ',
+                'You have not completed the placement process. To know what English and math courses are right for you, get started by visiting ',
+                'https://pasadena.edu/academics/support/counseling/academic-planning/placement.php'
+            ),
+            'https://pasadena.edu/academics/support/counseling/academic-planning/placement.php',
+            getButtonColor(checklist.assessment === "1"),
+            getIconProps(checklist.assessment === "1").name,
+            getIconProps(checklist.assessment === "1").color,
+            0
+        ),
+        createData(
+            ++id,
+            'Ed Plan',
+            checklist.edPlan,
+            createHoverText(
+                checklist.edPlan === "1",
+                'You have completed an educational plan. To view, please visit ',
+                'Completing an educational plan will make sure you’re taking the correct classes. (You can register without a plan) To request a plan use this link: ',
+                'https://pasadena.edu/academics/support/counseling/ask-a-counselor/new-students.php'
+            ),
+            'https://pasadena.edu/academics/support/counseling/academic-planning/',
+            getButtonColor(checklist.edPlan === "1"),
+            getIconProps(checklist.edPlan === "1").name,
+            getIconProps(checklist.edPlan === "1").color,
+            0
+        ),
+        createData(
+            ++id,
+            'Early Registration',
+            checklist.regPri,
+            checklist.regPri === "1" ? 'You have completed steps for early registration!' : 'To get registration priority, you must first complete the online orientation, assessment, and Ed Plan.',
+            'https://pasadena.edu/academics/support/counseling/academic-planning/placement.php',
+            getButtonColor(checklist.regPri === "1"),
+            getIconProps(checklist.regPri === "1").name,
+            getIconProps(checklist.regPri === "1").color,
+            0
+        )
+    ];
 }
 
 
@@ -249,7 +297,8 @@ MyInfo.propTypes = {
     classes: PropTypes.object.isRequired,
     cardControl: PropTypes.object.isRequired,
     data: PropTypes.object.isRequired,
-    cardInfo: PropTypes.object.isRequired
+    cardInfo: PropTypes.object.isRequired,
+    userInfo: PropTypes.object.isRequired
 }
 
 export default withStyles(styles)(MyInfo);
