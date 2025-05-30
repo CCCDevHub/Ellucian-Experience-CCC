ab095dddba6e75be4ea6ce6e99b41b0cc15425f1
hphan10 <hphan10@pasadena.edu>
2025-05-30 14:05:57 -0700

    Add Section Add Authorization Code card and update extension configuration

--- extension.js
+++ extension.js
@@ -1,15 +1,120 @@
 module.exports = {
-    "name": "Extension Name",
-    "publisher": "Your Name",
-    "cards": [{
-        "type": "TestExtCard",
-        "source": "./src/cards/TestExtCard",
-        "title": "Card Title",
-        "displayCardType": "Card Type",
-        "description": "Card Description",
-        "pageRoute": {
-            "route": "/",
-            "excludeClickSelectors": ['a']
+    name: "Section Add Authorization Code",
+    publisher: "Huey Phan",
+    cards: [{
+        type: "GraphQlQueryCard",
+        source: "./src/cards/Section-Authorization-Code",
+        title: "Section Add Authorization Code",
+        displayCardType: "Section Add Authorization Code",
+        description: "Section Add Authorization Code",
+        pageRoute: {
+            route: "/",
+            excludeClickSelectors: ['a']
+        },
+        configuration: {
+            client: [{
+                key: 'pipelineAPI',
+                label: 'Pipeline API',
+                type: 'text'
+            },
+            ],
+            server: [{
+                key: 'ethosApiKey',
+                label: 'Ethos API',
+                type: 'password',
+                required: true
+            }]
+        },
+        queries: {
+            "section-list": [
+                {
+                    "resourceVersions": {
+                        "sections": { min: 16 },
+                        "courses": { min: 16 },
+                        "subjects": { min: 6 },
+                        "sectionStatuses": { min: 11 },
+                        "academicPeriods": { min: 16 },
+                        "sectionInstructors": { min: 10 },
+                        "persons": { min: 12 },
+                        "instructionalMethods": { min: 6 }
+                    },
+                    "query":
+                        `query sectionList($personId: ID){
+                            	sectionInstructors: {sectionInstructors} (
+                                    filter: {
+                                        {instructor@persons} : { id: { EQ: $personId } }
+                                    }
+                                    sort: {{section@sections}:{{reportingAcademicPeriod@academicPeriods}:{code:DESC}}}
+                                ) {
+                                    edges {
+                                        node {
+                                            id
+                                            instructionalMethod6 {
+                                                title
+                                                abbreviation
+                                            }
+                                            section16 {
+                                                id
+                                                startOn
+                                                endOn
+                                                gradeSubmitted
+                                                titles {
+                                                    value
+                                                }
+                                                code
+                                                reportingAcademicPeriod16 {
+                                                    code
+                                                    title
+                                                    registration
+                                                }
+                                                maxEnrollment
+                                                crossListed
+                                                alternateIds {
+                                                    title
+                                                    value
+                                                }
+                                                course16 {
+                                                    subject: {subject} {
+                                                        abbreviation
+                                                    }
+                                                    number
+                                                }
+                                            }
+                                            instructor12 {
+                                                id
+                                                names {
+                                                    fullName
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                        }`
+                }
+            ],
+            'term-list': [
+                {
+                    resourceVersions: {
+                        academicPeriods: { min: 16 }
+                    },
+                    query:
+                        `query termList{
+                            academicPeriods: {academicPeriods} (
+                                filter: { code: { STARTS_WITH: "2" } }
+                                sort: { startOn: DESC }
+                            ) {
+                                totalCount
+                                edges {
+                                node {
+                                    id
+                                    code
+                                    title
+                                }
+                                }
+                            }
+                        }`
+                }
+            ]
         }
     }],
     "page": {
