285640b2cd1c8a6b8e9878fb5770de43e4228bbb
hphan10 <hphan10@pasadena.edu>
2026-03-24 10:54:31 -0700

    Refactor code structure for improved readability and maintainability

--- extension.js
+++ extension.js
@@ -1,18 +1,18 @@
 module.exports = {
-    "name": "Extension Name",
-    "publisher": "Your Name",
-    "cards": [{
-        "type": "TestExtCard",
-        "source": "./src/cards/TestExtCard",
-        "title": "Card Title",
-        "displayCardType": "Card Type",
-        "description": "Card Description",
-        "pageRoute": {
+    name: "Extension Name",
+    publisher: "Your Name",
+    cards: [{
+        type: "TestExtCard",
+        source: "./src/cards/TestExtCard",
+        title: "Card Title",
+        displayCardType: "Card Type",
+        description: "Card Description",
+        pageRoute: {
             "route": "/",
             "excludeClickSelectors": ['a']
         }
     }],
-    "page": {
-        "source": "./src/page/router.jsx"
+    page: {
+        source: "./src/page/router.jsx"
     }
 }
