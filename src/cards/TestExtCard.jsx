8b3d9c46e1c141427a273820d6f246c8ed449740
hphan10 <hphan10@pasadena.edu>
2026-05-29 11:51:20 -0700

    updated

--- src/cards/TestExtCard.jsx
+++ src/cards/TestExtCard.jsx
@@ -1,22 +1,25 @@
-import { withStyles } from '@ellucian/react-design-system/core/styles';
 import { spacing40 } from '@ellucian/react-design-system/core/styles/tokens';
-import { Typography, TextLink } from '@ellucian/react-design-system/core';
+import { makeStyles, Typography, TextLink } from '@ellucian/react-design-system/core';
 import { useCardControl, useCardInfo, useExtensionControl, useUserInfo, useData, useDashboardInfo } from '@ellucian/experience-extension-utils';
-import PropTypes from 'prop-types';
 import React, { useEffect, useMemo, useState } from 'react';
 import classnames from 'classnames';
 import { Icon } from '@ellucian/ds-icons/lib';
 
-const styles = () => ({
+const useStyles = makeStyles()({
     card: {
         marginTop: 0,
         marginRight: spacing40,
         marginBottom: 0,
         marginLeft: spacing40
+    },
+    spacing: {
+        marginBottom: spacing40
     }
 });
 
-function TestExtCard({ classes }) {
+
+const TestExtCard = () => {
+    const { classes } = useStyles();
     return (
         <div className={classes.card}>
             <Typography variant="h2">
@@ -26,8 +29,5 @@ function TestExtCard({ classes }) {
     );
 }
 
-TestExtCard.propTypes = {
-    classes: PropTypes.object.isRequired
-};
 
-export default withStyles(styles)(TestExtCard);
+export default TestExtCard;
